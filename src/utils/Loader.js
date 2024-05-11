7fb87b8eaffb48e17ff66553895bf4e1d96fae28
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2024-05-11 21:47:35 +0600

    title change, spinner change

--- src/utils/Loader.js
+++ src/utils/Loader.js
@@ -2,5 +2,7 @@ import React from "react";
 import "../css/loader.css";
 
 export const Loader = () => {
-    return <div className="loader"></div>;
+    return <div className=" spinner-grow text-info" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
 };
